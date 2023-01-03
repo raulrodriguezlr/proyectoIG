4a16136215b5cd8dd8cb858c9ffe2f8478f235c8
raulrodriguezlr <12rulo12@gmail.com>
2023-01-03 18:44:36 +0100

    Merge

---
+++ cmake-build-debug/CMakeFiles/proyectoFinal.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Raul/Desktop/IG/proyectoIG/src/Model.cpp" "CMakeFiles/proyectoFinal.dir/src/Model.cpp.obj" "gcc" "CMakeFiles/proyectoFinal.dir/src/Model.cpp.obj.d"
+  "C:/Users/Raul/Desktop/IG/proyectoIG/src/Shaders.cpp" "CMakeFiles/proyectoFinal.dir/src/Shaders.cpp.obj" "gcc" "CMakeFiles/proyectoFinal.dir/src/Shaders.cpp.obj.d"
+  "C:/Users/Raul/Desktop/IG/proyectoIG/src/main.cpp" "CMakeFiles/proyectoFinal.dir/src/main.cpp.obj" "gcc" "CMakeFiles/proyectoFinal.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/proyectoFinal.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for proyectoFinal.
+# This may be replaced when dependencies are built.
